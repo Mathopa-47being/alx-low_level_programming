abf29709552863c9636f8da8d7cabece6857d483
Mathopa <Mathopa.tlabela@gmail.com>
2023-02-20 06:31:32 -0800

    6size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
- * Return 0 (Success)
+ * Main - Entry point
+ *
+ * Return - Always 0 (Success)
  */
 int main(void)
 {
