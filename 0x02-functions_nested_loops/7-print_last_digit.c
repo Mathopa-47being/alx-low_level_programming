e9121782928a37cad5c1ed7b41bc113c131f6ff0
Mathopa <Mathopa.tlabela@gmail.com>
2023-02-22 12:53:37 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,13 @@
  * @n: the number
  * Return: value of the last digit
  */
-	int main(void)
+	int print_last_digit(int n)
 {
-	int r;
+	int ld = n % 10;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
 	return (0);
 }
