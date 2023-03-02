39ce489be953cedd52cd32c0502c383b26ddf626
Mathopa <Mathopa.tlabela@gmail.com>
2023-03-02 15:36:51 -0800

    Cap Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 					index++;
 
-					if (str[index - 1] == ' ' ||
+				if (str[index - 1] == ' ' ||
 					str[index - 1] == '\t' ||
 					str[index - 1] == '\n' ||
 					str[index - 1] == ',' ||
@@ -30,7 +30,7 @@
 					str[index - 1] == '}' ||
 					index == 0)
 						str[index] -= 32;
-
+					
 				index++;
 	}
 
